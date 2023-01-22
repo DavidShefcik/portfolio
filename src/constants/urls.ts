fc719e99aca77cde48954716e0b9d24efca611f4
DavidShefcik <davidshefcik@gmail.com>
2023-01-22 17:12:21 -0600

    resume page

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,4 +1,5 @@
 export enum ExternalUrls {
   GITHUB = "https://github.com/DavidShefcik",
   LINKEDIN = "https://www.linkedin.com/in/davidshefcik/",
+  RESUME = "https://docs.google.com/document/d/1TxLJcmJUdsyhkJuxIfgQbiehkqSFidhFzIfVz1FYwy0/edit?usp=sharing",
 }
